edc95849a1f3871ca30e5dc1eef2cdfac004ef15
yushangxiao <2644482241@qq.com>
2025-11-19 23:31:28 +0800

    update models

--- config/model.go
+++ config/model.go
@@ -4,9 +4,9 @@ var ModelReverseMap = map[string]string{}
 var ModelMap = map[string]string{
 	"claude-4-5-sonnet":       "claude45sonnet",
 	"claude-4.5-sonnet-think": "claude45sonnetthinking",
-	"gemini-2.5-pro-06-05":    "gemini2flash",
+	"gemini-3-pro":            "gemini30pro",
 	"o3-pro":                  "o3pro",
-	"gpt-5":                   "gpt5",
+	"gpt-5.1":                 "gpt51",
 	"gpt-5-think":             "gpt5_thinking",
 	"claude-4.1-opus-think":   "claude41opusthinking",
 }
